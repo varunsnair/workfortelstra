3f0e59eca2e3634d53ff076f682107ab8a81b499
varunsnair <varunsnair@gmail.com>
2017-05-24 17:39:57 +1000

    Update replacetext.sh

--- replacetext.sh
+++ replacetext.sh
@@ -1,3 +1,17 @@
-#! bin/bash
+#!/bin/bash
 
-sed -i '/line q/c\ varun' file.txt
+function escape_slashes {
+    sed 's/\//\\\//g' 
+}
+
+function change_line {
+    local OLD_LINE_PATTERN=$1; shift
+    local NEW_LINE=$1; shift
+    local FILE=$1
+
+    local NEW=$(echo "${NEW_LINE}" | escape_slashes)
+    sed -i .bak '/'"${OLD_LINE_PATTERN}"'/s/.*/'"${NEW}"'/' "${FILE}"
+    mv "${FILE}.bak" /tmp/
+}
+
+change_line "line q" "This line is removed by the varun." /home/ec2-user/file.txt
